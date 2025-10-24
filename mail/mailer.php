c6597aae1b81ad7ce06dbaa30759ff17b7edfe92
vitaliihapon <v.gapon@smileexpo.com.ua>
2025-10-24 10:54:17 -0600

    Fix sending message form

--- mail/mailer.php
+++ mail/mailer.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Email configuration
     $to = "gaponvv007@gmail.com";
-    $subject = "New Contact Form Submission";
+    $subject = "Hapon Portfolio Contact Form Submission";
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
